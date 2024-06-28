f1fbb139e7718820d8adead273ae9573b4b0e94c
saber son <elmohandesstamera@gmail.com>
2024-06-29 02:56:35 +0300

    setupt The Project

---
+++ lib/core/styles/app_text_styles.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jefit/core/helpers/extensions.dart';
+
+class AppTextStyles {
+  // Primary Color
+  TextStyle designer16MediumPrimary(BuildContext context) => TextStyle(
+        fontSize: 16.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.primary,
+      );
+
+  TextStyle designer18MediumPrimary(BuildContext context) => TextStyle(
+        fontSize: 18.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.primary,
+      );
+  TextStyle designe20MediumPrimary(BuildContext context) => TextStyle(
+        fontSize: 20.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.primary,
+      );
+  TextStyle designe22MediumPrimary(BuildContext context) => TextStyle(
+        fontSize: 22.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.primary,
+      );
+
+  TextStyle designe24MediumPrimary(BuildContext context) => TextStyle(
+        fontSize: 24.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.primary,
+      );
+
+  TextStyle designe26MediumPrimary(BuildContext context) => TextStyle(
+        fontSize: 26.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.primary,
+      );
+
+  /// onPrimary Color
+
+  TextStyle designer16MediumOnPrimary(BuildContext context) => TextStyle(
+        fontSize: 16.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.onPrimary,
+      );
+
+  TextStyle designer18MediumOnPrimary(BuildContext context) => TextStyle(
+        fontSize: 18.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.onPrimary,
+      );
+  TextStyle designe20MediumOnPrimary(BuildContext context) => TextStyle(
+        fontSize: 20.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.onPrimary,
+      );
+  TextStyle designe22MediumOnPrimary(BuildContext context) => TextStyle(
+        fontSize: 22.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.onPrimary,
+      );
+
+  TextStyle designe24MediumOnPrimary(BuildContext context) => TextStyle(
+        fontSize: 24.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.onPrimary,
+      );
+
+  TextStyle designe26MediumOnPrimary(BuildContext context) => TextStyle(
+        fontSize: 26.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.onPrimary,
+      );
+
+  // onSurface Color
+  TextStyle designer16MediumOnSurface(BuildContext context) => TextStyle(
+        fontSize: 16.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.onSurface,
+      );
+
+  TextStyle designer18MediumOnSurface(BuildContext context) => TextStyle(
+        fontSize: 18.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.onSurface,
+      );
+  TextStyle designe20MediumOnSurface(BuildContext context) => TextStyle(
+        fontSize: 20.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.onSurface,
+      );
+  TextStyle designe22MediumOnSurface(BuildContext context) => TextStyle(
+        fontSize: 22.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.onSurface,
+      );
+
+  TextStyle designe24MediumOnSurface(BuildContext context) => TextStyle(
+        fontSize: 24.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.onSurface,
+      );
+
+  TextStyle designe26MediumOnSurface(BuildContext context) => TextStyle(
+        fontSize: 26.sp,
+        fontWeight: FontWeight.w500,
+        color: context.theme.colorScheme.onSurface,
+      );
+}
--- lib/jefit_app.dart
+++ lib/jefit_app.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jefit/config/theme/dark_theme_manager.dart';
 
 class JefitApp extends StatelessWidget {
   const JefitApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Jefit App'),
-      ),
-      body: const Center(
-        child: Text('Hello World'),
-      ),
-    ));
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: DarkThemeManager.darkTheme,
+          home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Jefit App'),
+            ),
+            body: const Center(
+              child: Text('Hello World'),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
